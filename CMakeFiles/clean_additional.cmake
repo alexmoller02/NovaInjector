b19cfaa9fc632aa83cdb45826d5c5788d18bc9e1
Alex <alexmoller02@hotmail.com>
2026-07-09 15:09:04 +0200

    v.1.0.3

--- CMakeFiles/clean_additional.cmake
+++ CMakeFiles/clean_additional.cmake
@@ -1,7 +1,7 @@
 # Additional clean files
 cmake_minimum_required(VERSION 3.16)
 
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Release")
   file(REMOVE_RECURSE
   "CMakeFiles\\NovaInjector_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\NovaInjector_autogen.dir\\ParseCache.txt"
